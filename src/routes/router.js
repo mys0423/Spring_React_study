0f0bd0dc997cb224b5d46f42893b1691fe690936
mys0423 <mmmys2001@gmail.com>
2026-04-17 17:43:38 +0900

    feat: day11

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import MemberList from '../pages/member/MemberList';
 import MemberInfo from '../pages/member/MemberInfo';
+import MemberJoin from '../pages/member/MemberJoin';
+import MemberLogin from '../pages/member/MemberLogin';
 
 const router = createBrowserRouter ([
   {
@@ -9,8 +11,16 @@ const router = createBrowserRouter ([
     element: <MemberList />
   },
   {
-    path: "/members/member-info",
+    path: "/members/member-info/:id",
     element: <MemberInfo />
+  },
+  {
+    path: "/members/member-join",
+    element: <MemberJoin />
+  },
+  {
+    path: "/members/member-login",
+    element: <MemberLogin />
   },
 ])
 
